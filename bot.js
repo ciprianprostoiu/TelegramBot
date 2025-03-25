4149c87c9842496047953501bb6228beb6d595a2
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-03-25 11:53:16 +0100

    parte del progetto

---
+++ bot.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const TelegramBot= require('node-telegram-bot-api')
+const conf = JSON.parse(fs.readFileSync('conf.json'));
+const token = conf.key
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on("message", (msg)=> {
+    const chatId= msg.chat.id;
+    const text = msg.text;
+
+    if (text === "/start"){
+        bot.sendMessage(chatId, "Hello world");
+    }
+    else if (text === "/frocio"){
+        bot.sendMessage(chatId, "Tomi gay");
+    }
+});
