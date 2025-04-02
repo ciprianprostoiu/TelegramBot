afa2278ae5e0eb800aedb8697dbb236b715e72e6
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-04-02 18:13:50 +0200

    progetto finito

--- bot.js
+++ bot.js
@@ -19,25 +19,58 @@ bot.on("message", (msg)=> {
         const help="Comandi disponibili: \n /ingredienti == inserisci ingredienti con cui vuoi cucinare e ti consigliamo le migliori ricette \n /pasti == ti cosiglia i migliori pasti da cucinare in base alla tipologia (cena,pranzo,colazione) \n /allergeni == ti consiglia i migliori pasti senza gli alimenti a cui sei allergico"
         bot.sendMessage(chatId, help);
     }
-    else if(text === "/ingredienti"){
-        bot.sendMessage(chatId, "Inserisci ingredienti separati da una virgola es: cocco,pomodoro,carota");
-        bot.once("message", async(msg)=> {
+    else if (text === "/ingredienti") {
+        bot.sendMessage(chatId, "Inserisci ingredienti separati da una virgola es: tomato,potato,carrot");
+        bot.once("message", async (msg) => {
+
             const ingredienti = msg.text;
-            bot.sendMessage(chatId, "non è pronto!")
-        })
-    }
-    else if(text === "/pasti"){
-        bot.sendMessage(chatId, "Inserisci il tipo di pasto (cena,pranzo,colazione)");
-        bot.once("message", async(msg)=> {
+            const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredienti}&apiKey=${ApiKey}`;
+            
+            const response = await fetch(url);
+            const data = await response.json();
+                
+            if (data.length > 0) {
+                const ricette = data.map(r => `Nome Ricetta: ${r.title}\nLink: https://spoonacular.com/recipes/${r.title.replace(/\s+/g, '-')}-${r.id}`).join('\n\n');
+                bot.sendMessage(chatId, ricette);
+            } else {
+                bot.sendMessage(chatId, "Nessuna ricetta trovata con questi ingredienti.");
+            }
+        });
+    } 
+    else if (text === "/pasti") {
+        bot.sendMessage(chatId, "Inserisci il tipo di pasto (lunch, dinner, breakfast)");
+        bot.once("message", async (msg) => {
+
             const pasto = msg.text;
-            bot.sendMessage(chatId, "non è pronto!")
-        })
-    }
-    else if(text === "/allergeni"){
-        bot.sendMessage(chatId, "Inserisci allergeni separati da una virgola es: cocco,pomodoro,carota");
-        bot.once("message", async(msg)=> {
+            const url = `https://api.spoonacular.com/recipes/complexSearch?type=${pasto}&number=6&apiKey=${ApiKey}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data.results.length > 0) {
+                const ricette = data.results.map(element => `Nome Ricetta: ${element.title}\nLink: https://spoonacular.com/recipes/${element.title.replace(/\s+/g, '-')}-${element.id}`).join('\n\n');
+                bot.sendMessage(chatId, ricette);
+            } else {
+                bot.sendMessage(chatId, "Nessuna ricetta trovata per questo tipo di pasto.");
+            }
+        });
+    } 
+    else if (text === "/allergeni") {
+        bot.sendMessage(chatId, "Inserisci allergeni separati da una virgola es: tomato,potato,carrot");
+        bot.once("message", async (msg) => {
+
             const allergeni = msg.text;
-            bot.sendMessage(chatId, "non è pronto!")
-        })
+            const url = `https://api.spoonacular.com/recipes/complexSearch?intolerances=${allergeni}&number=6&apiKey=${ApiKey}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+                
+            if (data.results.length > 0) {
+                const ricette = data.results.map(r => `Nome Ricetta: ${r.title}\nLink: https://spoonacular.com/recipes/${r.title.replace(/\s+/g, '-')}-${r.id}`).join('\n\n');                    bot.sendMessage(chatId, ricette);
+            } else {
+                bot.sendMessage(chatId, "Nessuna ricetta trovata senza questi allergeni.");
+            }
+        });
     }
 });
